af265d4afad3fe02ba8d2c0a3b559ed86901d039
Daniel Kurniawan <dekaslr8@gmail.com>
2019-06-13 22:38:03 +0700

    done rkpd

--- application/controllers/RKPD.php
+++ application/controllers/RKPD.php
@@ -1,10 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Rpkd extends CI_Controller {
+class RKPD extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('rpkd/index');
+		$this->load->view('rkpd/index');
 	}
 }
